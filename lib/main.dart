538ef2baf18f80f9c1a79c050545ee913d9ba3ba
adi-kulkarni1 <61598391+adi-kulkarni1@users.noreply.github.com>
2021-04-22 15:34:22 -0500

    Updated Name and Description on Web
    pushed to hosting

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //removes the red debug banner
-      title: 'EPHS Club System',
+      title: 'Club Management System',
       home: AuthScreen(),
       theme: ThemeData(
         // This is the theme of your application.
